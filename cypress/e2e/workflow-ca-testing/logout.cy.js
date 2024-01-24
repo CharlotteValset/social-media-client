f167fd4deae548884585f37a4de3fe9dfc918b7e
CharlotteValset <119309299+CharlotteValset@users.noreply.github.com>
2024-01-24 13:44:33 +0100

    Testing: add multiple clicks to cypress tests

--- cypress/e2e/workflow-ca-testing/logout.cy.js
+++ cypress/e2e/workflow-ca-testing/logout.cy.js
@@ -4,17 +4,19 @@ describe("logout", () => {
     cy.clearAllLocalStorage();
 
     cy.wait(2000);
-    cy.get("button[data-bs-target='#loginModal']:visible").click();
+    cy.get("button[data-bs-target='#loginModal']:visible").click({
+      multiple: true,
+    });
     cy.wait(2000);
     cy.get("#loginModal #loginEmail")
       .should("exist")
       .type("charlie123@noroff.no");
     cy.get("#loginModal #loginPassword").should("exist").type("charlie12345");
-    cy.get("button[type='submit']:visible").click();
+    cy.get("button[type='submit']:visible").click({ multiple: true });
     cy.get(window.localStorage.getItem("profile")).should("not.be.empty");
     cy.get(window.localStorage.getItem("token")).should("not.be.empty");
     cy.wait(2000);
-    cy.get("button[data-auth='logout']:visible").click();
+    cy.get("button[data-auth='logout']:visible").click({ multiple: true });
     cy.wait(3000);
     cy.window().its("localStorage.profile").should("not.exist");
     cy.window().its("localStorage.token").should("not.exist");
