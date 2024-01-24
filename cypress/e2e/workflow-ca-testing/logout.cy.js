9914ec2b5ae2381543122ee9898b0c0a3ba3f4f6
CharlotteValset <119309299+CharlotteValset@users.noreply.github.com>
2024-01-24 13:59:04 +0100

    testing automated e2e test with githubAction

--- cypress/e2e/workflow-ca-testing/logout.cy.js
+++ cypress/e2e/workflow-ca-testing/logout.cy.js
@@ -1,4 +1,4 @@
-describe("logout", () => {
+/* describe("logout", () => {
   it("can log out with the logout button", () => {
     cy.visit("index.html");
     cy.clearAllLocalStorage();
@@ -20,5 +20,36 @@ describe("logout", () => {
     cy.wait(3000);
     cy.window().its("localStorage.profile").should("not.exist");
     cy.window().its("localStorage.token").should("not.exist");
+  });
+}); */
+
+describe("Logout", () => {
+  beforeEach(() => {
+    cy.visit("/");
+    cy.clearLocalStorage();
+  });
+
+  it("will log out", () => {
+    cy.visit("/");
+    cy.wait(1000);
+    cy.get(".btn-close:visible").click();
+    cy.wait(500);
+    cy.get("button[data-auth='login']:visible").click();
+    cy.wait(1500);
+    cy.get("input[type='email']:visible")
+      .should("exist")
+      .type("charlie123@noroff.no");
+    cy.get("input[type='password']:visible")
+      .should("exist")
+      .type("charlie12345");
+    cy.get(".btn-success:visible").click();
+    cy.wait(3000);
+    cy.then(
+      () => expect(window.localStorage.getItem("profile")).to.not.be.null,
+    );
+    cy.then(() => expect(window.localStorage.getItem("token")).to.not.be.null);
+    cy.get("button[data-auth='logout']").click();
+    cy.then(() => expect(window.localStorage.getItem("profile")).to.be.null);
+    cy.then(() => expect(window.localStorage.getItem("token")).to.be.null);
   });
 });
