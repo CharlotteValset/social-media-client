42b336d20f96d123d72ece8ae005ad016f744b78
CharlotteValset <119309299+CharlotteValset@users.noreply.github.com>
2024-01-25 12:47:05 +0100

    trying to fix auto e2e test

--- cypress/e2e/workflow-ca-testing/logout.cy.js
+++ cypress/e2e/workflow-ca-testing/logout.cy.js
@@ -4,20 +4,25 @@ describe("logout", () => {
     cy.clearAllLocalStorage();
 
     cy.wait(2000);
-    cy.get("button[data-bs-target='#loginModal']:visible").click({
-      multiple: true,
+    cy.get("form[id='registerForm'").within(() => {
+      cy.get("button[data-bs-target='#loginModal']:visible").click({
+        multiple: true,
+      });
     });
+    cy.wait(1000);
     cy.get("#loginModal #loginEmail")
       .should("exist")
       .type("charlie123@noroff.no");
+    cy.wait(500);
     cy.get("#loginModal #loginPassword").should("exist").type("charlie12345");
+    cy.wait(500);
     cy.get("button[type='submit']:visible").click({ multiple: true });
     cy.wait(2000);
     cy.get(window.localStorage.getItem("profile")).should("not.be.empty");
     cy.get(window.localStorage.getItem("token")).should("not.be.empty");
     cy.wait(2000);
     cy.get("button[data-auth='logout']:visible").click({ multiple: true });
-    cy.wait(3000);
+    cy.wait(2000);
     cy.window().its("localStorage.profile").should("not.exist");
     cy.window().its("localStorage.token").should("not.exist");
   });
