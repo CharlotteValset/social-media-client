cdbbf4f2c44cc1f3845bf3f4e0d13bf3a7fe652f
CharlotteValset <119309299+CharlotteValset@users.noreply.github.com>
2024-01-25 12:58:34 +0100

    Trying to fix auto e2e test

--- cypress/e2e/workflow-ca-testing/login.cy.js
+++ cypress/e2e/workflow-ca-testing/login.cy.js
@@ -4,15 +4,20 @@ describe("login", () => {
     cy.clearAllLocalStorage();
 
     cy.wait(2000);
-    cy.get("button[data-bs-target='#loginModal']:visible").click({
-      multiple: true,
+    cy.get("form[id='registerForm'").within(() => {
+      cy.get("button[data-bs-target='#loginModal']:visible").click({
+        multiple: true,
+      });
     });
-    cy.wait(2000);
+    cy.wait(1000);
     cy.get("#loginModal #loginEmail")
       .should("exist")
       .type("charlie123@noroff.no");
+    cy.wait(500);
     cy.get("#loginModal #loginPassword").should("exist").type("charlie12345");
+    cy.wait(500);
     cy.get("button[type='submit']:visible").click({ multiple: true });
+    cy.wait(2000);
     cy.get(window.localStorage.getItem("profile")).should("not.be.empty");
     cy.get(window.localStorage.getItem("token")).should("not.be.empty");
   });
@@ -22,15 +27,20 @@ describe("login", () => {
     cy.clearAllLocalStorage();
 
     cy.wait(2000);
-    cy.get("button[data-bs-target='#loginModal']:visible").click({
-      multiple: true,
+    cy.get("form[id='registerForm'").within(() => {
+      cy.get("button[data-bs-target='#loginModal']:visible").click({
+        multiple: true,
+      });
     });
+    cy.wait(1000);
     cy.get("#loginModal #loginEmail")
       .should("exist")
       .type("charlie123@gmail.no");
+    cy.wait(500);
     cy.get("#loginModal #loginPassword").should("exist").type("12345");
+    cy.wait(500);
     cy.get("button[type='submit']:visible").click({ multiple: true });
-    cy.wait(3000);
+    cy.wait(2000);
     cy.window().its("localStorage.profile").should("not.exist");
     cy.window().its("localStorage.token").should("not.exist");
   });
@@ -40,16 +50,20 @@ describe("login", () => {
     cy.clearAllLocalStorage();
 
     cy.wait(2000);
-    cy.get("button[data-bs-target='#loginModal']:visible").click({
-      multiple: true,
+    cy.get("form[id='registerForm'").within(() => {
+      cy.get("button[data-bs-target='#loginModal']:visible").click({
+        multiple: true,
+      });
     });
-    cy.wait(2000);
+    cy.wait(1000);
     cy.get("#loginModal #loginEmail")
       .should("exist")
       .type("charlie123@noroff.no");
+    cy.wait(500);
     cy.get("#loginModal #loginPassword").should("exist").type("12345");
+    cy.wait(500);
     cy.get("button[type='submit']:visible").click({ multiple: true });
-    cy.wait(4000);
+    cy.wait(2000);
     cy.window().its("localStorage.profile").should("not.exist");
     cy.window().its("localStorage.token").should("not.exist");
   });
